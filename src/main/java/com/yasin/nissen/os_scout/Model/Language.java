ff75ec82207c6e32ab290387225968da1593485c
Yasin <yasin-nissen@gmx.de>
2017-09-04 20:25:48 +0200

    alles neu

---
+++ src/main/java/com/yasin/nissen/os_scout/Model/Language.java
@@ -0,0 +1,20 @@
+package com.yasin.nissen.os_scout.Model;
+
+/**
+ * Created by ynissen on 8/12/17.
+ */
+public class ProjectLanguage {
+    private String codeLanguage;
+
+    public ProjectLanguage(String codeLanguage) {
+        this.codeLanguage = codeLanguage;
+    }
+
+    public String getCodeLanguage() {
+        return codeLanguage;
+    }
+
+    public void setCodeLanguage(String codeLanguage) {
+        this.codeLanguage = codeLanguage;
+    }
+}
